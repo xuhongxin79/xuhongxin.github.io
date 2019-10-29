5e0473053e0c575b65d1c093f3e7259c5d50226e
xuhongxin79 <46701281+xuhongxin79@users.noreply.github.com>
2019-10-29 13:50:31 +0800

    Update page.js

--- js/page.js
+++ js/page.js
@@ -2,3 +2,15 @@ function main()
 {
 ap("<div>main</div>",jid("show"),1);
 }
+function fx()  
+{  
+ap("<div>fx</div>",jid("show"),1);  
+}  
+function txl()
+{
+ap("<div>通信录</div>",jid("show"),1);
+}
+function my()
+{
+ap("<div>个人中心</div>",jid("show"),1);
+}
