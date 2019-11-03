e7daa4d84d65b66fc2b575b2049156e2f42b6ffc
xuhongxin79 <46701281+xuhongxin79@users.noreply.github.com>
2019-11-03 14:23:05 +0800

    Update byethostpg.js

--- js/byethostpg.js
+++ js/byethostpg.js
@@ -5,3 +5,25 @@ h+='</div>';
 ap(h,jid("show"),1);
 
 }
+function txl(){
+var h='<div>';
+h+='txl';
+h+='</div>';
+ap(h,jid("show"),1);
+
+}
+function fx(){
+var h='<div>';
+h+='fx';
+h+='</div>';
+ap(h,jid("show"),1);
+
+}
+function my(){
+var h='<div>';
+h+='my';
+h+='</div>';
+ap(h,jid("show"),1);
+
+}
+
