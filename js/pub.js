5c73ca6501b37a8cd1bba29198cc446c0a6f544e
xuhongxin79 <46701281+xuhongxin79@users.noreply.github.com>
2019-11-04 12:16:53 +0800

    Update pub.js

--- js/pub.js
+++ js/pub.js
@@ -1,4 +1,29 @@
 window.H={};
+function cookie_get(c_name)
+{
+if (document.cookie.length>0)   { 
+	c_start=document.cookie.indexOf(c_name + "=");    
+	if (c_start!=-1)     {        
+		c_start=c_start + c_name.length+1;    
+		c_end=document.cookie.indexOf(";",c_start);     
+		if(c_end==-1){c_end=document.cookie.length;}   
+		return unescape(document.cookie.substring(c_start,c_end));  
+	}    
+}
+return ""
+}
+function cookie_set(c_name,value,expiredays)
+{
+var exdate=new Date()
+exdate.setDate(exdate.getDate()+expiredays)
+document.cookie=c_name+ "=" +escape(value)+
+((expiredays==null) ? "" : ";expires="+exdate.toGMTString())
+}
+//临时传递数据
+function temporary(str)
+{ 	
+	cookie_set("temporary",str,1);
+}
 function getBody()
 {
 if(document.body)
