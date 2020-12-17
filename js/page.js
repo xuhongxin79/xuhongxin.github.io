301e49fbc6b0b1d5fe317925b37c7590ce65cca9
xuhongxin79 <46701281+xuhongxin79@users.noreply.github.com>
2020-12-17 12:44:59 +0800

    Create page.js

--- js/page.js
+++ js/page.js
@@ -2,6 +2,7 @@ function main()
 {
 var str=lista({"t1":"跑腿实例","url":"https://xuhongxin79.github.io"});
   str+=lista({"t1":"社交软件","url":"http://hongxin.byethost15.com"});
+   str+=lista({"t1":"仓库","url":"https://github.com/xuhongxin79"});
 ap(str,jid("show"),1);
 }
 function fx()  
